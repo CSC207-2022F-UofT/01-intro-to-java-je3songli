9939b1858a777419b2fbef758953cbec09373d5c
je3songli <jessicali9@yahoo.ca>
2023-05-16 10:10:08 -0400

    Completed Tasks 2 and 3

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -62,6 +62,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
+        int my_variable = 100;
 
 
 
@@ -99,6 +100,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for (int i = 10; i >= 0; i--){
+            System.out.println("Current count: " + i);
+        }
 
 
     }
